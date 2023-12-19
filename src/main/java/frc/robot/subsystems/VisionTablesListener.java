78e3380c0eae68231699e3e94b52109a56df71f5
Dynamic6448 <134905030+jprock23@users.noreply.github.com>
2023-12-19 16:55:27 -0600

    Attempted to fix network tables
    No success

--- src/main/java/frc/robot/subsystems/VisionTablesListener.java
+++ src/main/java/frc/robot/subsystems/VisionTablesListener.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.IntegerArraySubscriber;
+import edu.wpi.first.networktables.IntegerSubscriber;
+import edu.wpi.first.networktables.IntegerTopic;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -10,32 +12,39 @@ public class VisionTablesListener {
 
     private NetworkTableInstance networkTable;
     private NetworkTable visionTable;
-    private IntegerArraySubscriber tagIDSub;
-    private IntegerArraySubscriber xCoordsSub;
-    private IntegerArraySubscriber yCoordsSub;
-    private IntegerArraySubscriber xEulerSub;
-    private IntegerArraySubscriber yEulerSub;
-    private IntegerArraySubscriber zEulerSub;
+    final IntegerArraySubscriber tagIDSub;
+    // private IntegerArraySubscriber xCoordsSub;
+    // private IntegerArraySubscriber yCoordsSub;
+    // private IntegerArraySubscriber xEulerSub;
+    // private IntegerArraySubscriber yEulerSub;
+    // private IntegerArraySubscriber zEulerSub;
 
     public VisionTablesListener() {
         networkTable = NetworkTableInstance.getDefault();
         visionTable = networkTable.getTable("Vision");
-        SmartDashboard.putString("Ansh", "Ansh");
-        tagIDSub = visionTable.getIntegerArrayTopic("IDs").subscribe(new long[] {});
-        xCoordsSub = visionTable.getIntegerArrayTopic("X Coords").subscribe(new long[] {});
-        yCoordsSub = visionTable.getIntegerArrayTopic("Y Coords").subscribe(new long[] {});
-        xEulerSub = visionTable.getIntegerArrayTopic("X Euler Angles").subscribe(new long[] {});
-        yEulerSub = visionTable.getIntegerArrayTopic("Y Euler Angles").subscribe(new long[] {});
-        zEulerSub = visionTable.getIntegerArrayTopic("Z Euler Angles").subscribe(new long[] {});
+        tagIDSub = visionTable.getIntegerArrayTopic("Tag Ids").subscribe(new long[] {});
+        // xCoordsSub = visionTable.getIntegerArrayTopic("X Coords").subscribe(new
+        // long[] {});
+        // yCoordsSub = visionTable.getIntegerArrayTopic("Y Coords").subscribe(new
+        // long[] {});
+        // xEulerSub = visionTable.getIntegerArrayTopic("X Euler Angles").subscribe(new
+        // long[] {});
+        // yEulerSub = visionTable.getIntegerArrayTopic("Y Euler Angles").subscribe(new
+        // long[] {});
+        // // zEulerSub = visionTable.getIntegerArrayTopic("Z Euler
+        // Angles").subscribe(new long[] {});
     }
 
     public void putInfoOnDashboard() {
         SmartDashboard.putNumberArray("Tag IDs", convertArray(tagIDSub.get()));
-        SmartDashboard.putNumberArray("X Coords", convertArray(xCoordsSub.get()));
-        SmartDashboard.putNumberArray("Y Coords", convertArray(yCoordsSub.get()));
-        SmartDashboard.putNumberArray("X Euler Angles", convertArray(xEulerSub.get()));
-        SmartDashboard.putNumberArray("Y Euler Angles", convertArray(yEulerSub.get()));
-        SmartDashboard.putNumberArray("Z Euler Angles", convertArray(zEulerSub.get()));
+        // SmartDashboard.putNumberArray("X Coords", convertArray(xCoordsSub.get()));
+        // SmartDashboard.putNumberArray("Y Coords", convertArray(yCoordsSub.get()));
+        // SmartDashboard.putNumberArray("X Euler Angles",
+        // convertArray(xEulerSub.get()));
+        // SmartDashboard.putNumberArray("Y Euler Angles",
+        // convertArray(yEulerSub.get()));
+        // SmartDashboard.putNumberArray("Z Euler Angles",
+        // convertArray(zEulerSub.get()));
         SmartDashboard.putNumber("Ansh", 0);
     }
 
@@ -44,9 +53,9 @@ public class VisionTablesListener {
     private double[] convertArray(long[] arr) {
         double[] newArr = new double[arr.length];
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++)
             newArr[i] = (double) arr[i];
-        }
+
         return newArr;
     }
 
